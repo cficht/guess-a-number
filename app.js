c6a4cb37f4023580967495dc827de9ac57e3e62c
cficht <chris.ficht@gmail.com>
2020-01-14 19:12:06 -0800

    made app.js function as intended.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import compareNumbers from './compareNumbers.js';
+
 const numberInput = document.getElementById('number-input');
 const submitButton = document.getElementById('submit-button');
 
@@ -5,7 +7,31 @@ const triesRemaining = document.getElementById('tries-remaining');
 const highLow = document.getElementById('high-low');
 const winLose = document.getElementById('win-lose');
 
-const numberGuess = Math.round(Math.random() * 10);
+const numberToGuess = Math.round(Math.random() * 10);
 let triesCount = 4;
 
-console.log(numberGuess);
+submitButton.addEventListener('click', () => {
+    const numberUserGuess = Number(numberInput.value);
+    let submittedTry = compareNumbers(numberUserGuess, numberToGuess);
+
+    if (submittedTry === 1) {
+        triesCount--;
+        highLow.textContent = 'How high? Too high!';
+    } else if (submittedTry === -1) {
+        triesCount--;
+        highLow.textContent = 'Too low, bro!';
+    } else {
+        winLose.textContent = 'YOOOOOOU WIN!';
+        highLow.textContent = 'The right stuff!';
+        submitButton.disabled = true;
+        return;
+    }
+
+    triesRemaining.textContent = triesCount;
+
+    if (triesCount <= 0) {
+        winLose.textContent = 'YOOOOU LOSE';
+        submitButton.disabled = true;
+        return;
+    }
+});
