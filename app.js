192acb61c028db4661c8bc8c1860a3214a428260
cficht <chris.ficht@gmail.com>
2020-01-14 21:31:17 -0800

    added UI work into functions

--- app.js
+++ app.js
@@ -7,29 +7,40 @@ const triesRemaining = document.getElementById('tries-remaining');
 const highLow = document.getElementById('high-low');
 const winLose = document.getElementById('win-lose');
 
-const numberToGuess = Math.round(Math.random() * 10);
+const numberToGuess = Math.ceil(Math.random() * 20); // used Math.ceil to ensure the number is never zero;
 let triesCount = 4;
 
 submitButton.addEventListener('click', () => {
     const numberUserGuess = Number(numberInput.value);
     let submittedTry = compareNumbers(numberUserGuess, numberToGuess);
-
+    
     if (submittedTry === 1) {
         triesCount--;
         highLow.textContent = 'How high? Too high!';
+        highLow.style.verticalAlign = '10px';
     } else if (submittedTry === -1) {
         triesCount--;
         highLow.textContent = 'Too low, bro!';
+        highLow.style.verticalAlign = '-10px';
     } else {
-        winLose.textContent = 'YOOOOOOU WIN!!!';
+        triesCount--;
         highLow.textContent = 'The right stuff!';
+        highLow.style.verticalAlign = '0px';
+        winLose.textContent = 'YOOOOOOU WIN!!!';
+        winLose.style.color = 'gold';
         submitButton.disabled = true;
+        numberInput.disabled = true;
+        triesRemaining.textContent = triesCount;
+        return;
     }
 
     triesRemaining.textContent = triesCount;
+    triesRemaining.style.fontSize = `${11 + (triesCount * 4)}px`;
 
     if (triesCount <= 0) {
-        winLose.textContent = 'YOOOOU LOSE!!!';
+        winLose.textContent = 'YOOOOOOU LOSE!!!';
+        winLose.style.color = 'red';
         submitButton.disabled = true;
+        numberInput.disabled = true;
     }
 });
